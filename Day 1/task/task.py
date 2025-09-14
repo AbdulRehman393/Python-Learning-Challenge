9173b0a6f3a71032380af97ece51700933eba317
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-14 14:53:30 +0500

    Add files via upload

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+#Band Name Generator
+#This program asks user their city and pet name , based on that , it suggests band name.
+print("Welcome to the Band Name Generator🎸")
+city = input("What is the name of the city where you grew up?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " +pet)
