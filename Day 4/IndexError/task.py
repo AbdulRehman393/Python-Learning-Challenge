2d00da6bde8a7167f7f4256238ca86f76133f650
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-15 22:45:26 +0500

    Add files via upload

---
+++ Day 4/IndexError/task.py
@@ -0,0 +1,8 @@
+
+
+#Nested List = List within list, that's called Nested List.
+fruits = ["Cherry", "Apple", "Pear"]
+veg = ["Cucumber", "Kale", "Spinnach"]
+
+fruits_and_veg =[fruits,veg]
+print(fruits_and_veg)
